1f3fbf87494fb70f90558af2bf603dff76e7a6d2
Seanlxx256 <seanlxx256@gmail.com>
2024-10-03 01:21:53 -0400

    upgrade to Flask-SQLAlchemy

--- project/schema.sql
+++
@@ -1,9 +0,0 @@
--- This schema will set up a table entries with three fields: id, title, and text.
-
-drop table if exists entries;
-
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  text text not null
-);
